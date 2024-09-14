4802427bf70e298727f6edd9441e8c111f4177b2
Aditya-Dawadikar <mail.aditya.dawadikar@gmail.com>
2024-09-14 14:21:12 -0700

    Add: Console based text output screenshots

--- src/runner.py
+++ src/runner.py
@@ -10,18 +10,24 @@ from src.visualization import (plot_energy_expenditure_per_step,
 
 def exec_runner(verbose:bool=False,
                 export_gif:bool=False)->pd.DataFrame:
+    print("=================================")
+    print("SIMULATION LOGS")
+    print("=================================")
     sim_name, performance, meta_data = run_simulation(verbose=verbose, export_gif = export_gif)
-    
-    print(f"""Simulation {sim_name} completed in {meta_data["time_elapsed"]}s""")
+
+    print(f"""Simulation {sim_name} completed in {round(meta_data["time_elapsed"],2)}s""")
  
     print("=================================")
-
-    print(meta_data["moves_made"])
-
-
+    print("AGENT MOVES")
+    print("=================================")
+    for move in meta_data["moves_made"]:
+        print(move)
+    print("=================================")
+    print("AGENT PERFORMANCE")
     print("=================================")
     for elem in performance["performance_metrics"]:
         print(f"""{elem}:{performance["performance_metrics"][elem]}""")
+    print("=================================")
 
     plot_battery_usage(meta_data["battery_usage"])
     plot_energy_expenditure_per_step(meta_data["step_energy_expenditure"])
